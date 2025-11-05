3aecb9ffe53bac7c95e39a9f54aa1722ea07774e
Nikita <06nekit13@gmail.com>
2025-11-05 20:10:54 +0600

    added toolbar

---
+++ frontend/src/PageNotFound.tsx
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+
+const PageNotFound = () => {
+  return (
+    <Typography variant="h1" textAlign="center" mt={10}>
+      Page not found
+    </Typography>
+  );
+};
+
+export default PageNotFound;

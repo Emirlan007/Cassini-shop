d4d3d40e535b09cfc75b56662f0529d46bdd168d
FridiMori <fridim389@gmail.com>
2025-11-05 17:29:02 +0600

    Added a product schema (version 1 without categories)

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -5,11 +5,15 @@ import { UsersController } from './users/users.controller';
 import { ConfigModule } from '@nestjs/config';
 import { IsUserExistsValidator } from './validators/is-user-exists.validator';
 import { AuthService } from './auth/auth.service';
+import { Product, ProductSchema } from './schemas/product.schema';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost/cassini-shop'),
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema },
+      { name: Product.name, schema: ProductSchema },
+    ]),
     ConfigModule.forRoot({ isGlobal: true }),
   ],
   controllers: [UsersController],

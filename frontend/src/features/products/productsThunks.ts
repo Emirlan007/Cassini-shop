5fface21e0114c8b9e23eed0f8042496e74b358d
Nikita <06nekit13@gmail.com>
2025-11-05 16:24:04 +0600

    added thunks and global state for products

---
+++ frontend/src/features/products/productsThunks.ts
@@ -0,0 +1,89 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import type { IGlobalError, Product, ProductInput } from "../../types";
+import { axiosApi } from "../../axiosApi";
+import { isAxiosError } from "axios";
+
+export const fetchProducts = createAsyncThunk<
+  Product[],
+  void,
+  { rejectValue: IGlobalError }
+>("products/fetchAll", async (_, { rejectWithValue }) => {
+  try {
+    const { data } = await axiosApi.get<Product[]>("/products");
+    return data;
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      return rejectWithValue(error.response.data);
+    }
+
+    throw error;
+  }
+});
+
+export const fetchProductById = createAsyncThunk<
+  Product,
+  string,
+  { rejectValue: IGlobalError }
+>("products/fetchOne", async (id, { rejectWithValue }) => {
+  try {
+    const { data: product } = await axiosApi.get<Product>("/products/" + id);
+    return product;
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      return rejectWithValue(error.response.data);
+    }
+
+    throw error;
+  }
+});
+
+export const createProduct = createAsyncThunk<
+  Product,
+  ProductInput,
+  { rejectValue: IGlobalError }
+>("products/create", async (productData, { rejectWithValue }) => {
+  try {
+    const formData = new FormData();
+
+    formData.append("name", productData.name);
+    formData.append("price", String(productData.price));
+
+    if (productData.sizes) {
+      formData.append("sizes", productData.sizes);
+    }
+
+    if (productData.description) {
+      formData.append("description", productData.description);
+    }
+
+    if (productData.images) {
+      for (const image of productData.images) {
+        formData.append("images", image);
+      }
+    }
+
+    if (productData.video) {
+      formData.append("video", productData.video);
+    }
+
+    const { data: product } = await axiosApi.post<Product>(
+      "/products",
+      formData
+    );
+
+    return product;
+  } catch (error) {
+    if (isAxiosError(error) && error.response) {
+      return rejectWithValue(error.response.data);
+    }
+
+    throw error;
+  }
+});
+
+export const deleteProduct = createAsyncThunk<void, string>(
+  "products/delete",
+  async (id) => {
+    await axiosApi.delete("/products/" + id);
+  }
+);

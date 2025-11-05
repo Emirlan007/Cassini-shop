a65b968fb517258a575d14ff2dc05836bac45f1e
FridiMori <fridim389@gmail.com>
2025-11-05 17:29:02 +0600

    Added a product schema (version 1 without categories)

---
+++ api/src/schemas/product.schema.ts
@@ -0,0 +1,30 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type ProductDocument = Product & Document;
+
+@Schema()
+export class Product {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop({ type: [String], required: true })
+  colors: string[];
+
+  @Prop()
+  description?: string;
+
+  @Prop({ type: [String], required: true })
+  size: string[];
+
+  @Prop({ required: true })
+  price: number;
+
+  @Prop()
+  video?: string;
+
+  @Prop()
+  image?: string;
+}
+
+export const ProductSchema = SchemaFactory.createForClass(Product);

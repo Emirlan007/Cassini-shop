5679e5d9f4cd4892d0b568db9376079ab697b85b
GLOBAL <abdashimov99@mail.ru>
2025-11-05 09:17:15 -0500

    [api] intalled @types/multer created file-upload.interceprot.ts, file-upload.module.ts, file-upload.service.ts multer.config.ts.
    configured multer in order to get avatar or images and save it in public/files
    
    added gitignore in order not to save files for now

--- api/config.ts
+++ api/config.ts
@@ -1,8 +1,13 @@
 import { configDotenv } from 'dotenv';
-
+import path from "path";
 configDotenv();
+const rootPath = __dirname
+
+
 
 const config = {
+  rootPath,
+  publicPath:path.join(rootPath, "public"),
   google: {
     clientId: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -5,9 +5,11 @@ import { UsersController } from './users/users.controller';
 import { ConfigModule } from '@nestjs/config';
 import { IsUserExistsValidator } from './validators/is-user-exists.validator';
 import { AuthService } from './auth/auth.service';
+import { FileUploadModule } from './shared/file-upload/file-upload.module';
 
 @Module({
   imports: [
+    FileUploadModule,
     MongooseModule.forRoot('mongodb://localhost/cassini-shop'),
     MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
     ConfigModule.forRoot({ isGlobal: true }),

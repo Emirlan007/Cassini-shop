621445d03467f7e9f4cd1f63ebbfd5139c3cf4e6
Nikita <06nekit13@gmail.com>
2025-11-06 18:48:16 +0600

    added cors

--- api/src/main.ts
+++ api/src/main.ts
@@ -7,6 +7,10 @@ import { useContainer } from 'class-validator';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  });
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.useGlobalPipes(new ValidationPipe());
   useContainer(app.select(AppModule), { fallbackOnErrors: true });

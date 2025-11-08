7182fc1e77df7ad032650edbb4e8334d7f366784
Aidai Mashrapova <aidaimashrapova8@mail.ru>
2025-11-08 19:09:39 +0600

    interceptors improved

--- frontend/src/axiosApi.ts
+++ frontend/src/axiosApi.ts
@@ -1,22 +1,6 @@
 import axios from "axios";
 import { API_URL } from "./constants";
-import {type RootState, store} from "./app/store.ts";
 
 export const axiosApi = axios.create({
   baseURL: API_URL,
-});
-
-axiosApi.interceptors.request.use((config) => {
-    try {
-        const state: RootState = store.getState();
-        const token = state.users.user?.token;
-
-        if (token && config.headers) {
-            config.headers.Authorization = token;
-        }
-    } catch (e) {
-        console.error(e);
-    }
-
-    return config;
 });

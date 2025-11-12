59e97d4c33b77e06406707d60ddb15de7895845e
Nikita <06nekit13@gmail.com>
2025-11-12 16:51:27 +0600

    removed navigation from slider, added indicator to show current image

--- frontend/src/features/banners/BannersCarousel.tsx
+++ frontend/src/features/banners/BannersCarousel.tsx
@@ -1,81 +1,98 @@
-import {Swiper, SwiperSlide} from 'swiper/react';
-import {Alert, Box, Button, Typography} from '@mui/material';
-import { Navigation } from 'swiper/modules';
-import {useAppDispatch, useAppSelector} from "../../app/hooks.ts";
-import {Link} from 'react-router-dom';
-// @ts-ignore
-import 'swiper/css';
-// @ts-ignore
-import 'swiper/css/navigation';
-import './styles.css';
-import {selectBanners, selectBannersError} from "./bannersSlice.ts";
-import {useEffect} from "react";
-import {fetchBanners} from "./bannersThunks.ts";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Autoplay } from "swiper/modules";
+import { Alert, Box, Button, Typography } from "@mui/material";
+import { useAppDispatch, useAppSelector } from "../../app/hooks.ts";
+import { Link } from "react-router-dom";
+import { selectBanners, selectBannersError } from "./bannersSlice.ts";
+import { useEffect } from "react";
+import { fetchBanners } from "./bannersThunks.ts";
+import "swiper/swiper.css";
+import "swiper/swiper-bundle.css";
+import "./styles.css";
 
 const BannersCarousel = () => {
-    const dispatch = useAppDispatch();
-    const banners = useAppSelector(selectBanners);
-    const bannersError = useAppSelector(selectBannersError);
+  const dispatch = useAppDispatch();
+  const banners = useAppSelector(selectBanners);
+  const bannersError = useAppSelector(selectBannersError);
 
-    useEffect(() => {
-        dispatch(fetchBanners());
-    }, [dispatch]);
+  useEffect(() => {
+    dispatch(fetchBanners());
+  }, [dispatch]);
 
-    return (
-        <>
-            {
-                bannersError ?
-                    <Alert severity="error">{bannersError}</Alert> :
-                    <Box sx={{
-                        width: '100%',
-                        maxWidth: { lg: '1200px', md: '800px', sm: '600px', xs: '350px' },
-                        mx: 'auto',
-                        my: 8,
-                    }}>
-                        <Swiper navigation={true} modules={[Navigation]} className="mySwiper">
-                            {
-                                banners.map((banner) => (
-                                    <SwiperSlide key={banner._id}  >
-                                        <Box sx={{
-                                            width: '100%',
-                                            pr: { xs: '0px', md: '100px'},
-                                            height: '100%',
-                                            gap: '3',
-                                            display: 'flex',
-                                            flexDirection: 'column',
-                                            alignItems: {md: 'flex-end', xs: 'center'},
-                                            justifyContent: 'center',
-                                            backgroundRepeat: 'no-repeat',
-                                            backgroundPosition: 'center',
-                                            backgroundSize: 'contain',
-                                            backgroundImage: 'url(' + 'http://localhost:8000' + banner.image + ')',
-                                        }}>
+  return (
+    <>
+      {bannersError ? (
+        <Alert severity="error">{bannersError}</Alert>
+      ) : (
+        <Box
+          sx={{
+            width: "100%",
+            maxWidth: { lg: "1200px", md: "800px", sm: "600px", xs: "320px" },
+            mx: "auto",
+            mt: 1,
+            mb: 3,
+          }}
+        >
+          <Swiper
+            modules={[Pagination, Autoplay]}
+            pagination={{ clickable: true }}
+            autoplay={{ delay: 3000 }}
+            loop={true}
+            className="mySwiper"
+          >
+            {banners.map((banner) => (
+              <SwiperSlide key={banner._id}>
+                <Box
+                  sx={{
+                    width: "100%",
+                    pr: { xs: "0px", lg: "100px" },
+                    height: "100%",
+                    gap: "3",
+                    display: "flex",
+                    flexDirection: "column",
+                    alignItems: { sm: "center", md: "flex-end" },
+                    justifyContent: "center",
+                    backgroundRepeat: "no-repeat",
+                    backgroundPosition: "center",
+                    backgroundSize: "contain",
+                    backgroundImage:
+                      "url(" + "http://localhost:8000" + banner.image + ")",
+                  }}
+                >
+                  <Box
+                    sx={{
+                      backgroundColor: "#cccccc4b",
+                      borderRadius: 3,
+                      p: 2,
+                      backdropFilter: "blur(5px)",
+                      mx: 1,
+                    }}
+                  >
+                    <Typography variant="h4">{banner.title}</Typography>
 
-                                            <Typography variant="h4">{banner.title}</Typography>
+                    <Typography variant="h6">{banner.description}</Typography>
 
-                                            <Typography variant="h6">
-                                                {banner.description}
-                                            </Typography>
-
-                                            <Link
-                                                target="_blank"
-                                                rel="noopener noreferrer"
-                                                to={banner.link ? banner.link : '#'}
-                                            >
-                                                <Button variant='contained' sx={{color: 'white', textDecoration: 'none'}}>
-                                                    More info
-                                                </Button>
-                                            </Link>
-                                        </Box>
-                                    </SwiperSlide>
-                                ))
-                            }
-                        </Swiper>
-                    </Box>
-            }
-
-        </>
-    );
+                    <Link
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      to={banner.link ? banner.link : "#"}
+                    >
+                      <Button
+                        variant="contained"
+                        sx={{ color: "white", textDecoration: "none" }}
+                      >
+                        More info
+                      </Button>
+                    </Link>
+                  </Box>
+                </Box>
+              </SwiperSlide>
+            ))}
+          </Swiper>
+        </Box>
+      )}
+    </>
+  );
 };
 
 export default BannersCarousel;

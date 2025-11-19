55125a3735915a23e30bed4474ccfb45beaf36e3
FridiMori <fridim389@gmail.com>
2025-11-20 01:13:23 +0600

    Remove unused import

--- api/src/orders/orders.server.ts
+++ api/src/orders/orders.server.ts
@@ -1,4 +1,4 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { FileUploadService } from '../shared/file-upload/file-upload.service';

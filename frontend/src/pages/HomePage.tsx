cd7d8e671e9ad18c216d134d7a8d752fed9bf277
sumeiia-rgb <sumaiaamt@gmail.com>
2025-11-23 16:49:17 +0600

    added some common colors to theme and changed title in home page

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,16 +1,16 @@
 import { useEffect } from "react";
-import { Box, Stack } from "@mui/material";
+import {Box, Stack, Typography} from "@mui/material";
 import { useAppDispatch, useAppSelector } from "../app/hooks";
 import { fetchProducts } from "../features/products/productsThunks";
 import { selectProducts } from "../features/products/productsSlice";
 import ProductList from "../features/products/ProductsList.tsx";
 import BannersCarousel from "../features/banners/BannersCarousel.tsx";
-// import { useTranslation } from "react-i18next";
+import { useTranslation } from "react-i18next";
 
 const HomePage = () => {
     const dispatch = useAppDispatch();
     const products = useAppSelector(selectProducts);
-    // const {t} = useTranslation()
+    const {t} = useTranslation()
 
     useEffect(() => {
         dispatch(fetchProducts(undefined));
@@ -19,21 +19,33 @@ const HomePage = () => {
     return (
         <Box sx={{ backgroundColor: "#FFFFFF", minHeight: "100vh", py: { xs: 3, sm: 4 }}}>
             <Stack spacing={{ xs: 2, sm: 3 }} alignItems="center">
-                {/*<Typography*/}
-                {/*    variant="h4"*/}
-                {/*    sx={{*/}
-                {/*        color: "#660033",*/}
-                {/*        fontWeight: 600,*/}
-                {/*        textAlign: "center",*/}
-                {/*        fontSize: { xs: "1.75rem", sm: "2.125rem" },*/}
-                {/*        px: 2*/}
-                {/*    }}*/}
-                {/*>*/}
-                {/*    {t("popularProducts")}*/}
-                {/*</Typography>*/}
-
                 <Box>
                     <BannersCarousel />
+                </Box>
+                <Box
+                    alignSelf='flex-start'
+                    sx={{
+                        pl: {
+                            xl: 20,
+                            lg: 16,
+                            md: 12,
+                            sm: 8,
+                            xs: 4
+                        }
+                    }}
+                >
+                    <Typography
+                        variant="h4"
+                        sx={{
+                            fontWeight: 700,
+                            fontSize: '17px',
+                            lineHeight: '125%',
+                            letterSpacing: '-0.02em',
+                            color: '#111827',
+                        }}
+                    >
+                        {t("popularProducts")}
+                    </Typography>
                 </Box>
                 <ProductList products={products} />
             </Stack>

760261f883c527c29c31283a00b4127ff9f3accc
Nikita <06nekit13@gmail.com>
2025-12-16 16:46:55 +0600

    added product metrics fetching

--- api/src/analytics/analytics.service.ts
+++ api/src/analytics/analytics.service.ts
@@ -3,17 +3,33 @@ import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { Event } from 'src/analytics/schemas/event.schema';
 import { CreateEventDto } from './dto/create-event.dto';
+import { ProductStatsByDay } from './schemas/productStatsByDay.schema';
 
 @Injectable()
 export class AnalyticsService {
   constructor(
     @InjectModel(Event.name)
     private eventModel: Model<Event>,
+
+    @InjectModel(ProductStatsByDay.name)
+    private productStatsByDayModel: Model<ProductStatsByDay>,
   ) {}
 
   async trackEvent(dto: CreateEventDto) {
     await this.eventModel.create(dto);
 
     return { status: 'ok' };
+  }
+
+  async getProductMetrics(from: Date, to: Date) {
+    return this.productStatsByDayModel
+      .find({
+        date: {
+          $gte: from,
+          $lte: to,
+        },
+      })
+      .sort({ date: 1 })
+      .lean();
   }
 }

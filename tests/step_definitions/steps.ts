16930d6a130afe314903cbc4cb2d5554a42480eb
Aidai Mashrapova <aidaimashrapova8@mail.ru>
2026-01-16 19:01:15 +0600

    [test] Created tests for placing an order

--- tests/step_definitions/steps.ts
+++ tests/step_definitions/steps.ts
@@ -368,3 +368,79 @@ When("the user switches the language to {string}", (lang: string) => {
   I.click('[data-testid="language-select"]');
   I.click(`[data-testid="language-${lang}"]`);
 });
+
+Then("the user sees the product {string} on the page", (product: string) => {
+  I.see(product);
+});
+
+/////////////
+
+When('the user clicks "checkout-next-from-cart"', () => {
+  I.click('[data-testid="checkout-next-from-cart"]');
+  I.wait(2);
+});
+
+Then('the user sees the "checkout-form"', (step: string) => {
+  I.waitForElement('[data-testid="checkout-form"]', 5);
+});
+
+Then('the user sees the "payment-step"', (step: string) => {
+  I.waitForElement('[data-testid="payment-step"]', 5);
+  I.wait(2);
+});
+
+Then('checkout step "{string}" is active', async (step: string) => {
+  const isActive = await I.grabAttributeFrom(
+    `[data-testid="checkout-step-${step}"]`,
+    "data-active"
+  );
+
+  if (isActive !== "true") {
+    throw new Error(`Checkout step ${step} is not active`);
+  }
+});
+
+
+When("the user submits checkout form", () => {
+  I.click('[data-testid="checkout-submit"]');
+  I.wait(1);
+});
+
+Then('the user sees checkout validation errors', async () => {
+  await I.wait(1);
+  await I.waitForText('Пожалуйста, заполните все поля', 7);
+});
+
+Then('the user sees the message {string}', (message: string) => {
+  I.wait(1);
+  I.see(message);
+});
+
+When('the user chooses qrCode payment', () => {
+  I.click('[data-testid="payment-qr"]');
+});
+
+When('the user places the order', () => {
+  I.click('[data-testid="place-order-button"]');
+});
+
+Then('the user sees her order on the account page', () => {
+  I.waitForElement('[data-testid="user-orders-title"]', 5);
+  I.seeElement('[data-testid="order-card"]');
+});
+
+When('the user refreshes the {string} page', (page: string) => {
+  I.amOnPage(page);
+  I.refreshPage();
+});
+
+Then('only one order is displayed', async () => {
+  const ordersCount = await I.grabNumberOfVisibleElements('[data-testid="order-card"]');
+  if (ordersCount !== 1) {
+    throw new Error(`Expected 1 order, but found ${ordersCount}`);
+  }
+});
+
+Then('the order contains the product {string}', (product: string) => {
+  I.see(product, '[data-testid="order-card"]');
+});

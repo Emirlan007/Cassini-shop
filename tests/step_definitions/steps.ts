e25105e6bba44422d351fbc4160936e2bbb58d06
sumeiia-rgb <sumaiaamt@gmail.com>
2026-01-16 18:18:35 +0600

    increased wait element so tests would have time to check elements on the page

--- tests/step_definitions/steps.ts
+++ tests/step_definitions/steps.ts
@@ -2,35 +2,35 @@ const { I } = inject();
 
 Given("the user is on the {string} page", (page: string) => {
   I.amOnPage(page);
-  I.wait(2)
+  I.wait(5)
 });
 
 When(
   "the user enters {string} in the {string} field",
   (value: string, field: string) => {
     I.fillField(field, value);
-    I.wait(2)
+    I.wait(5)
   }
 );
 
 When("the form is submitted", () => {
   I.click('button[type="submit"]');
-  I.wait(2)
+  I.wait(5)
 });
 
 Then("the user is redirected to the {string} page", (page: string) => {
   I.waitInUrl(page, 1);
-  I.wait(2)
+  I.wait(5)
   I.seeCurrentUrlEquals("/");
-  I.wait(2)
+  I.wait(5)
 });
 
 Then("the user sees the message {string}", (message: string) => {
   I.see(message);
-  I.wait(2)
+  I.wait(5)
 });
 
 Then("the user stays on the {string} page", (page: string) => {
   I.seeCurrentUrlEquals(page);
-  I.wait(2)
+  I.wait(5)
 });

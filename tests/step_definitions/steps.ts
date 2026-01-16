b7f62cca7ca1d1ed3d663a7e0a90221d94701ed5
sumeiia-rgb <sumaiaamt@gmail.com>
2026-01-16 18:58:32 +0600

    added different methods

--- tests/step_definitions/steps.ts
+++ tests/step_definitions/steps.ts
@@ -2,35 +2,33 @@ const { I } = inject();
 
 Given("the user is on the {string} page", (page: string) => {
   I.amOnPage(page);
-  I.wait(5)
+  I.wait(2)
 });
 
 When(
   "the user enters {string} in the {string} field",
   (value: string, field: string) => {
     I.fillField(field, value);
-    I.wait(5)
+    I.wait(2)
   }
 );
 
 When("the form is submitted", () => {
   I.click('button[type="submit"]');
-  I.wait(5)
+  I.wait(2)
 });
 
 Then("the user is redirected to the {string} page", (page: string) => {
-  I.waitInUrl(page, 1);
-  I.wait(5)
-  I.seeCurrentUrlEquals("/");
-  I.wait(5)
+  I.waitInUrl(page, 5);
+  I.wait(2)
 });
 
 Then("the user sees the message {string}", (message: string) => {
-  I.see(message);
-  I.wait(5)
+  I.waitForText(message, 5);
+  I.wait(2)
 });
 
 Then("the user stays on the {string} page", (page: string) => {
-  I.seeCurrentUrlEquals(page);
-  I.wait(5)
+  I.waitInUrl(page, 5);
+  I.wait(2)
 });
